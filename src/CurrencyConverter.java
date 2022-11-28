7cb5ed2cc018a2736b185fef4aafee6629b2dd8d
Zhejie Jiang <zhejiej@nycstudents.net>
2022-11-28 12:28:33 -0500

    dkjfkdjfkdjfkdjkfd

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class CurrencyConverter {
+import java.util.Objects;
+
+public class CurrencyConverter {
+    String origCurrency;
+    int origAmount;
+    String convertedCurrency;
+    public CurrencyConverter(String currency, int amount, String newCurrency) {
+        if (currency.equals("1")) {
+            this.origCurrency = "euro";
+        } else if (currency.equals("2")) {
+            this.origCurrency = "usd";
+        } else if (currency.equals("3")) {
+            this.origCurrency = "china yuan";
+        } else if (currency.equals("4")) {
+            this.origCurrency = "turkish lira";
+        } else if (currency.equals("5")) {
+            this.origCurrency = "mexican peso";
+        } else if (currency.equals("6")) {
+            this.origCurrency = "thai baht";
+        } else if (currency.equals("7")) {
+            this.origCurrency = "pound sterling";
+        } else if (currency.equals("8")) {
+            this.origCurrency = "japanese yen";
+        } else if (currency.equals("9")) {
+            this.origCurrency = "vietnamese dong";
+        } else if (currency.equals("10")) {
+            this.origCurrency = "south korean won";
+        } else {
+            this.origCurrency = "canadian dollar";
+        }
+        this.origAmount = amount;
+        if (Objects.equals(newCurrency, "1")) {
+            this.convertedCurrency = "euro";
+        } else if (Objects.equals(newCurrency, "2")) {
+            this.convertedCurrency = "usd";
+        } else if (Objects.equals(newCurrency, "3")) {
+            this.convertedCurrency = "china yuan";
+        } else if (Objects.equals(newCurrency, "4")) {
+            this.convertedCurrency = "turkish lira";
+        } else if (Objects.equals(newCurrency, "5")) {
+            this.convertedCurrency = "mexican peso";
+        } else if (Objects.equals(newCurrency, "6")) {
+            this.convertedCurrency = "thai baht";
+        } else if (Objects.equals(newCurrency, "7")) {
+            this.convertedCurrency = "pound sterling";
+        } else if (Objects.equals(newCurrency, "8")) {
+            this.convertedCurrency = "japanese yen";
+        } else if (Objects.equals(newCurrency, "9")) {
+            this.convertedCurrency = "vietnamese dong";
+        } else if (Objects.equals(newCurrency, "10")) {
+            this.convertedCurrency = "south korean won";
+        } else {
+            this.convertedCurrency = "canadian dollar";
+        }
+    }
+    public double convertingRate() { //find how much each currency is worth in another country
+        if ()
+    }
+    public double getNewAmount() { //return new amount
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello welcome to Elias's currency converter!");
+        System.out.println("What would you like to do today?");
+        System.out.print("1. convert currency \n2. quit\n> ");
+        String input = scan.nextLine();
+        while (input == "") {
+            System.out.print("> ");
+            input = scan.nextLine();
+        }
+        if (input == "2") {
+            System.out.println("Thank you for using Elias's currency converter!\nGoodbye!");
+            System.exit(0);
+        }
+        while (input != "2") {
+            System.out.print("What currency would you like to convert?\n");
+            System.out.print("""
+                    1. Euro
+                    2. USD
+                    3. China Yuan
+                    4. Turkish Lira
+                    5. Mexican Peso
+                    6. Thai Baht
+                    7. Pound Sterling
+                    8. Japanese Yen
+                    9. Vietnamese dong
+                    10. South Korean Won
+                    11. Canadian Dollar""");
+            System.out.print("\n> ");
+            String currency = scan.nextLine();
+            while (!(currency.contains("1") || currency.contains("2") || currency.contains("3") || currency.contains("4") || currency.contains("5") || currency.contains("6") || currency.contains("7") || currency.contains("8") || currency.contains("9") || currency.contains("10") || currency.contains("11"))) {
+                System.out.print("Please answer correctly.\n> ");
+            }
+            System.out.print("Type how much you want to convert: ");
+            int money = scan.nextInt(); //if statement to make sure user input a valid answer
+            System.out.print("What currency would you like to convert it to?\n");
+            System.out.print("""
+                    1. Euro
+                    2. USD
+                    3. China Yuan
+                    4. Turkish Lira
+                    5. Mexican Peso
+                    6. Thai Baht
+                    7. Pound Sterling
+                    8. Japanese Yen
+                    9. Vietnamese dong
+                    10. South Korean Won
+                    11. Canadian Dollar""");
+            String newCurrency = scan.nextLine();
+            while (!(newCurrency.contains("1") || newCurrency.contains("2") || newCurrency.contains("3") || newCurrency.contains("4") || newCurrency.contains("5") || newCurrency.contains("6") || newCurrency.contains("7") || newCurrency.contains("8") || newCurrency.contains("9") || newCurrency.contains("10") || newCurrency.contains("11"))) {
+                System.out.print("Please answer correctly.\n> ");
+            }
+            CurrencyConverter converter = new CurrencyConverter(currency, money, newCurrency);
+
+        }
+    }
 }
